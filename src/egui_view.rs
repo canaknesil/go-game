c2d9f9454f9d4509223b4de5db034cb7804e5fe3
Can Aknesil <aknesil@kth.se>
2024-12-19 23:41:05 +0100

    turn stone

--- src/egui_view.rs
+++ src/egui_view.rs
@@ -3,7 +3,6 @@ use crate::model::{Model, Point, Stone, Turn};
 use eframe::egui;
 
 
-
 pub struct EguiView {
     model: Model
 }
@@ -47,7 +46,7 @@ impl EguiView {
 	eframe::run_simple_native("Go", options, move |ctx, _frame| {
 	    // TODO: menu
 	    
-	    egui::SidePanel::left("side_panel").show(ctx, |ui| {
+	    egui::SidePanel::left("side_panel").resizable(false).exact_width(200.0).show(ctx, |ui| {
 		ui.label("Mode:");
 		ui.radio_value(&mut mode, Mode::Setup, "Setup");
 		ui.radio_value(&mut mode, Mode::Game, "Game");
@@ -90,24 +89,45 @@ impl EguiView {
 		    }
 		}
 	    });
-	    
+
             egui::CentralPanel::default().show(ctx, |ui| {
 		//
-		// DRAW
+		// DRAW BOARD
 		//
 		
 		let painter = ui.painter();
 		let rect = ui.max_rect();
 
-		// Find the largest square A within the rect
-		let size = rect.width().min(rect.height());
-		let square_a = egui::Rect::from_center_size(rect.center(), egui::Vec2::splat(size));
+		// Assume a drawing canvas.
+		let board_size = self.model.get_board_size();
+		let margin_cell_size_ratio = 0.8;
+		let canvas_ratio = (board_size as f32 + 2.0 * margin_cell_size_ratio + 2.0) // Additional space of size cell_size on the right
+		    / (board_size as f32 + 2.0 * margin_cell_size_ratio);
+
+		let (canvas_width, canvas_height, canvas_top_left) = if rect.width() / rect.height() > canvas_ratio {
+		    // bigger width
+		    let h = rect.height();
+		    let w = h * canvas_ratio;
+		    (w, h, egui::Pos2::new(rect.left() + (rect.width() - w) / 2.0, rect.top()))
+		} else {
+		    // bigger height
+		    let w = rect.width();
+		    let h = w / canvas_ratio;
+		    (w, h, egui::Pos2::new(rect.left(), rect.top() + (rect.height() - h) / 2.0))
+		};
+
+		let canvas = egui::Rect::from_min_size(canvas_top_left, egui::Vec2::new(canvas_width, canvas_height));
+		painter.rect_filled(canvas, 0.0, egui::Color32::from_rgb(180, 180, 180));
+
+		// Draw board
+		let size = canvas_height;
+		let board_center = canvas_top_left + egui::Vec2::new(size / 2.0, size / 2.0);
+		let cell_size = canvas_height / (board_size as f32 + 2.0 * margin_cell_size_ratio);
+
+		let square_a = egui::Rect::from_center_size(board_center, egui::Vec2::splat(size));
 		painter.rect_filled(square_a, 0.0, egui::Color32::from_rgb(225, 225, 130));
 		
 		// Create square B with a margin inside square A
-		let board_size = self.model.get_board_size();
-		let margin_cell_size_ratio = 0.8;
-		let cell_size = square_a.width() / (board_size as f32 - 1.0 + 2.0 * margin_cell_size_ratio);
 		let margin = cell_size * margin_cell_size_ratio;
 		let square_b = square_a.shrink(margin);
 		let board_origin = square_b.left_top();
@@ -133,13 +153,17 @@ impl EguiView {
 
 		// Draw stones
 		let board = self.model.get_board();
+		let stone_radius_ratio = 0.4; // 0.5 makes the stones touch
+		let stone_radius = cell_size * stone_radius_ratio;
+		let black_stone_color = egui::Color32::from_rgb(0, 0, 0);
+		let white_stone_color = egui::Color32::from_rgb(255, 255, 255);
 
 		for col_idx in 0..board_size {
 		    for row_idx in 0..board_size {
 			let point = board.get(col_idx, row_idx).unwrap();
 			let stone_color = match point {
-			    Point::Black => Some(egui::Color32::from_rgb(0, 0, 0)), // Black stones
-			    Point::White => Some(egui::Color32::from_rgb(255, 255, 255)), // White stones
+			    Point::Black => Some(black_stone_color), // Black stones
+			    Point::White => Some(white_stone_color), // White stones
 			    Point::Empty => None, // No stone
 			};
 
@@ -148,15 +172,22 @@ impl EguiView {
 			    let y = square_b.top() + row_idx as f32 * cell_size;
 
 			    let center = egui::Pos2::new(x, y);
-			    let radius = cell_size * 0.4;
-
-			    painter.circle_filled(center, radius, color);
+			    painter.circle_filled(center, stone_radius, color);
 			}
 		    }
 		}
 
+		// Draw area on the right of the board
+		let turn = self.model.get_turn();
+		let turn_stone_center = egui::Pos2::new(square_a.right() + cell_size, canvas.top() + canvas_height / 2.0);
+		let turn_stone_color = match turn {
+		    Turn::Black => black_stone_color,
+		    Turn::White => white_stone_color
+		};
+		painter.circle_filled(turn_stone_center, stone_radius, turn_stone_color);
+
 		//
-		// GET INPUT
+		// GET MOUSE INPUT
 		//
 
 		// Handle clicks
